ce3125fca17232f61dc8c3cc8f3cba4652dcfee1
Rokas <rookas.rudzenskas@gmail.com>
2021-05-04 14:53:30 +0300

    Rokas

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ img_file = 'Car_Detector.png'
 
 classifier_file = 'cars.xml'
 
+# open the image
+img = cv2.imread(img_file)
 
+# display the detected car image
+cv2.imshow("Rokas Car Detector", img)
+
+# Dont autoclous until the key is pressed
+cv2.waitKey()
 
 
 
