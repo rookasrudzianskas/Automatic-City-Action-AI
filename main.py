4b45a509a861b5d3f8cf6350937858447cb6ac6d
Rokas <rookas.rudzenskas@gmail.com>
2021-05-04 15:01:32 +0300

    Rokas

--- main.py
+++ main.py
@@ -8,8 +8,18 @@ classifier_file = 'cars.xml'
 # open the image
 img = cv2.imread(img_file)
 
+
+#  create a car classifier
+
+car_tracker = cv2.CascadeClassifier(classifier_file)
+# black and white image
+black_n_white = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+
+
 # display the detected car image
-cv2.imshow("Rokas Car Detector", img)
+cv2.imshow("Rokas Car Detector", black_n_white)
 
 # Dont autoclous until the key is pressed
 cv2.waitKey()
